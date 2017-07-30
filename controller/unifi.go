4b019996514ee8f22ffed6beb7da23fe600f759f
Emil Hummel <emil@hummel.yt>
2017-07-30 17:49:52 +0200

    fix: Added UniFi client variable to UniFi controller

--- controller/unifi.go
+++ controller/unifi.go
@@ -1,7 +1,29 @@
 package controller
 
-import "net/http"
+import (
+	"github.com/sequoiia/unifi-proper-portal/model"
+	"github.com/sequoiia/unifi-proper-portal/unifi"
+	"log"
+	"net/http"
+)
+
+var Uclient *unifi.Client
 
 func UniFiCallback(w http.ResponseWriter, r *http.Request) {
+	if Uclient == nil {
+		Uclient = unifi.NewClient(&unifi.ClientConfig{
+			Username:        Config.UniFi.Username,
+			Password:        Config.UniFi.Password,
+			UniFiBaseUrlRaw: Config.UniFi.BaseUrl,
+			Site:            Config.UniFi.Site,
+			InsecureMode:    Config.UniFi.InsecureMode,
+		})
 
+		err := Uclient.Login()
+		if err != nil {
+			log.Println(err)
+		}
+	}
+
+	guestDetails := model.GetUniFiGuestFromCallback(r)
 }
