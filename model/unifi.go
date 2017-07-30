a1b6ff0883fb9e0b083a5e9efe77ebcfc589700f
Emil Hummel <sequoiia@hummel.yt>
2017-07-30 14:03:43 +0200

    Added UniFi guest model, renamed UniFi guest callback method

--- model/unifi.go
+++ model/unifi.go
@@ -14,7 +14,20 @@ type UniFiCallbackGuest struct {
 	Ssid                  string
 }
 
-func GetUniFiGuest(r *http.Request) UniFiCallbackGuest {
+type UniFiLoginRequest struct {
+	Username string `json:"username"`
+	Password string `json:"password"`
+}
+
+type UniFiGuestAuthoriseRequest struct {
+	Mac     string   `json:"mac"`
+	Minutes float64  `json:"minutes"`
+	Up      *float64 `json:"up,omitempty"`
+	Down    *float64 `json:"down,omitempty"`
+	Bytes   *float64 `json:"bytes,omitempty"`
+}
+
+func GetUniFiGuestFromCallback(r *http.Request) UniFiCallbackGuest {
 	u := UniFiCallbackGuest{}
 	u.ClientMacAddress = r.URL.Query().Get("id")
 	u.AccessPointMacAddress = r.URL.Query().Get("ap")
